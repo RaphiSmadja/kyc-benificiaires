996dc2bb58c88edd5bf7b4d58422ce1839b2d342
RaphiSmadja <raphael_smadja@hotmail.com>
2024-12-17 14:27:51 +0100

    first commit

---
+++ src/main/java/fr/bpifrance/kyc_beneficiaires/KycBeneficiairesApplication.java
@@ -0,0 +1,13 @@
+package fr.bpifrance.kyc_beneficiaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycBeneficiairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycBeneficiairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/bpifrance/kyc_beneficiaires/KycBeneficiairesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bpifrance.kyc_beneficiaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycBeneficiairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
